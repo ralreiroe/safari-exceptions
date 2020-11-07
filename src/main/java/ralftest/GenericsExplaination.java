a7f56312d2012509355d9a845a6c7623a860d409
Ralf Oenning <ralf@invicto.net>
2020-11-07 13:02:59 +0000

    generics in general instance ceation is when compiler needs to know the real types

--- src/main/java/ralftest/GenericsExplaination.java
+++ src/main/java/ralftest/GenericsExplaination.java
@@ -9,69 +9,73 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
-     interface Collector<T, A, R> {
-        Supplier<A> supplier();
+//declaring stuff - all generic
+interface Collector<T, A, R> {
+    Supplier<A> supplier();
 
-        BiConsumer<A, T> accumulator();
+    BiConsumer<A, T> accumulator();
 
-        BinaryOperator<A> combiner();
+    BinaryOperator<A> combiner();
 
-        Function<A, R> finisher();
+    Function<A, R> finisher();
 
-        Set<java.util.stream.Collector.Characteristics> characteristics();
+    Set<java.util.stream.Collector.Characteristics> characteristics();
+}
+
+//more declaring stuff - all generic
+class CollectorImpl<T, A, R> implements java.util.stream.Collector<T, A, R> {
+    private final Supplier<A> supplier;
+    private final BiConsumer<A, T> accumulator;
+    private final BinaryOperator<A> combiner;
+    private final Function<A, R> finisher;
+    private final Set<Characteristics> characteristics;
+
+    CollectorImpl(Supplier<A> supplier,
+                  BiConsumer<A, T> accumulator,
+                  BinaryOperator<A> combiner,
+                  Function<A, R> finisher,
+                  Set<Characteristics> characteristics) {
+        this.supplier = supplier;
+        this.accumulator = accumulator;
+        this.combiner = combiner;
+        this.finisher = finisher;
+        this.characteristics = characteristics;
     }
+
+    @Override
+    public BiConsumer<A, T> accumulator() {
+        return accumulator;
+    }
+
+    @Override
+    public Supplier<A> supplier() {
+        return supplier;
+    }
+
+    @Override
+    public BinaryOperator<A> combiner() {
+        return combiner;
+    }
+
+    @Override
+    public Function<A, R> finisher() {
+        return finisher;
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return characteristics;
+    }
+
+};
+
+
 public class GenericsExplaination {
-
-
-    public static class CollectorImpl<T, A, R> implements java.util.stream.Collector<T, A, R> {
-        private final Supplier<A> supplier;
-        private final BiConsumer<A, T> accumulator;
-        private final BinaryOperator<A> combiner;
-        private final Function<A, R> finisher;
-        private final Set<Characteristics> characteristics;
-
-        CollectorImpl(Supplier<A> supplier,
-                      BiConsumer<A, T> accumulator,
-                      BinaryOperator<A> combiner,
-                      Function<A, R> finisher,
-                      Set<Characteristics> characteristics) {
-            this.supplier = supplier;
-            this.accumulator = accumulator;
-            this.combiner = combiner;
-            this.finisher = finisher;
-            this.characteristics = characteristics;
-        }
-
-        @Override
-        public BiConsumer<A, T> accumulator() {
-            return accumulator;
-        }
-
-        @Override
-        public Supplier<A> supplier() {
-            return supplier;
-        }
-
-        @Override
-        public BinaryOperator<A> combiner() {
-            return combiner;
-        }
-
-        @Override
-        public Function<A, R> finisher() {
-            return finisher;
-        }
-
-        @Override
-        public Set<Characteristics> characteristics() {
-            return characteristics;
-        }
-
-    };
-
 
     public static void main(String[] args) {
 
+        //now we are creating objects
+        //the compiler needs to  generic types  replaced with real types
         final List<String> oldStaffDoesNotHaveConsistentTokens = List.of("reserve-adjustment.metrics", "Other");
         final List<String> activeFlagsCanBeMissing = List.of("active");
         Stream<List<String>> str = Stream.of(oldStaffDoesNotHaveConsistentTokens, activeFlagsCanBeMissing);
@@ -79,7 +83,7 @@ public class GenericsExplaination {
 
         List<String> collect = stringStream.collect(
                 new CollectorImpl<String, List<String>, List<String>>(
-                        () -> new ArrayList(),  //binds Supplier<A> to ArrayList<String>
+                        () -> new ArrayList(),  //binds Supplier<A> to Supplier<ArrayList<String>>
                         (a, s) -> a.add(s),
                         (a1, a2) -> {
                             a1.addAll(a2);
